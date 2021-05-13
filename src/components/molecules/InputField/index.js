77168bbc32cee96aa68581ba8060fbab3be3d701
Michal Janiec <michal.janiec95@gmail.com>
2021-05-13 23:12:34 +0200

    Created InputField to simplyfing form

---
+++ src/components/molecules/InputField/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { InputWrapper, StyledLabel, StyledField } from './styled';
+
+const InputField = ({
+  label,
+  name,
+  component,
+  type,
+  placeholder,
+  options,
+  step,
+  min,
+  max,
+}) => {
+  return (
+    <InputWrapper>
+      <StyledLabel htmlFor={name}>{label}:</StyledLabel>
+      {component === 'select' ? (
+        <StyledField name={name} component="select" id={name} required>
+          <option />
+          {options.map((option) => (
+            <option value={option}>{option}</option>
+          ))}
+        </StyledField>
+      ) : (
+        <StyledField
+          id={name}
+          name={name}
+          component={component}
+          type={type}
+          placeholder={placeholder}
+          step={step}
+          min={min}
+          max={max}
+          required
+        />
+      )}
+    </InputWrapper>
+  );
+};
+
+export default InputField;
