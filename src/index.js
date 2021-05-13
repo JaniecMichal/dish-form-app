502c17722b7aff0449cedb592660fae0cd0c0c16
Michal Janiec <michal.janiec95@gmail.com>
2021-05-13 23:09:44 +0200

    Added redux Provider to project with store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import App from 'views/App';
 import { theme } from 'assets/Theme/theme';
 import { GlobalStyle } from 'assets/GlobalStyles/GlobalStyles';
+import store from 'store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
