ef466767008852020bd222b877a9e2ef2a9dbecd
Michal Janiec <michal.janiec95@gmail.com>
2021-05-13 23:29:11 +0200

    changed background color and font color

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
   font-size: ${({ theme }) => theme.fontSize.normal};
-  color: ${({ theme }) => theme.colors.mainText};
+  color: ${({ theme }) => theme.colors.buttonText};
   background-color: ${({ theme }) => theme.colors.teal};
   border: 2px solid ${({ theme }) => theme.colors.scoripion};
   box-shadow: 8px 8px 15px 2px rgba(0, 0, 0, 0.21);
