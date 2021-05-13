7e8cb2de3f776d78505f7f6e74ff7256288ab6be
Michal Janiec <michal.janiec95@gmail.com>
2021-05-13 23:11:51 +0200

    Created Title component

---
+++ src/components/atoms/Title/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  font-weight: 700px;
+  font-size: ${({ theme }) => theme.fontSize.big};
+  color: ${({ theme }) => theme.colors.mainText};
+  padding-bottom: 5px;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.scoripion};
+`;
