550ad87c22fefa6d49833c4ef0a7cbb1f04501d7
Michal Janiec <michal.janiec95@gmail.com>
2021-05-15 01:17:16 +0200

    Added media - changed font-Size

--- src/components/atoms/Title/index.js
+++ src/components/atoms/Title/index.js
@@ -6,4 +6,8 @@ export const Title = styled.h2`
   color: ${({ theme }) => theme.colors.mainText};
   padding-bottom: 5px;
   border-bottom: 2px solid ${({ theme }) => theme.colors.scoripion};
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    font-size: ${({ theme }) => theme.fontSize.normal};
+  }
 `;
