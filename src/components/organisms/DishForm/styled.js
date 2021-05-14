1faf868d83cab26fe34871527556a6ccb4ee94a4
Michal Janiec <michal.janiec95@gmail.com>
2021-05-15 00:51:22 +0200

    Changed dimensions

--- src/components/organisms/DishForm/styled.js
+++ src/components/organisms/DishForm/styled.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 export const FormWrapper = styled.form`
-  width: 800px;
-  height: 600px;
-  max-width: 100%;
+  width: 100%;
+  max-width: 800px;
   padding: 20px 30px;
   background-color: ${({ theme }) => theme.colors.mainBackground};
   font-size: 18px;
