6132fe51c70cc79393d8e6afda29ede5d3fd023b
Michal Janiec <michal.janiec95@gmail.com>
2021-05-15 01:17:30 +0200

    Added media

--- src/components/molecules/InputField/styled.js
+++ src/components/molecules/InputField/styled.js
@@ -6,14 +6,35 @@ export const InputWrapper = styled.div`
   grid-template-columns: 0.4fr 1fr;
   align-items: center;
   margin-bottom: 20px;
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `;
 
 export const StyledLabel = styled.label`
   font-size: ${({ theme }) => theme.fontSize.normal};
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    font-size: ${({ theme }) => theme.fontSize.small};
+    margin-bottom: 5px;
+  }
 `;
 
 export const StyledField = styled(Field)`
   width: 400px;
   padding: 10px 20px;
   border: 2px solid ${({ theme }) => theme.colors.grey};
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    width: 300px;
+    padding: 10px 10px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.ekstraMobile}px) {
+    width: 200px;
+    padding: 10px 10px;
+  }
 `;
