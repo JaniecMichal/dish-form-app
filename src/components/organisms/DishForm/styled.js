4598a591f9b1f5bec040c24dcfe93176b6d4534a
Michal Janiec <michal.janiec95@gmail.com>
2021-05-15 01:17:41 +0200

    Added media

--- src/components/organisms/DishForm/styled.js
+++ src/components/organisms/DishForm/styled.js
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 export const FormWrapper = styled.form`
   width: 100%;
   max-width: 800px;
+  margin-top: 10%;
   padding: 20px 30px;
   background-color: ${({ theme }) => theme.colors.mainBackground};
   font-size: 18px;
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}px) {
+    margin-top: 20px;
+  }
 `;
